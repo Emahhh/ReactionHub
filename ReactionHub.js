b98bcce93bdc0e3c8d5085bfffac10a416f28a2b
Emahhh <buonaccorsiemanuele@gmail.com>
2021-06-20 02:02:53 +0200

    added the transpiled js from ts

---
+++ ReactionHub.js
@@ -0,0 +1,26 @@
+// ReactionHub
+// mettere nella page lista operatori https://developer.twitter.com/en/docs/twitter-api/v1/rules-and-filtering/search-operators
+// se vuoi fare just one of these keywords fai un'altra funzione che trasforma keyword in keyword con OR
+function createQuery(keyword, accounts) {
+    var queryAccounts = "";
+    for (var _i = 0, accounts_1 = accounts; _i < accounts_1.length; _i++) {
+        var e = accounts_1[_i];
+        queryAccounts += "OR from:", queryAccounts += e, queryAccounts += ", ";
+    }
+    return "filter:media ".concat(keyword, " (", queryAccounts, ")");
+}
+function createURL(query) {
+    return "https://twitter.com/search?q=".concat(encodeURI(query));
+}
+function search(accounts) {
+    window.open(createURL(createQuery(document.getElementById("typed").value, accounts)));
+}
+function searchByChecked() {
+    search(["findurmeme"]);
+}
+// prova
+var accounts = ["findurmeme", "allreactionvids"];
+var keyword = "hey";
+console.log(accounts.toString());
+var myquery = createQuery(keyword, accounts);
+console.log(createURL(myquery));
